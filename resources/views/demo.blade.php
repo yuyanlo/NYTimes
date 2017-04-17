0fb1022125ea3a94acc638c655585c13207a2994
Rita Lo <ritalo@Ritade-MacBook-Pro.local>
2017-04-17 18:17:03 +0800

    prototype for this project, but the query string still be 'trump'.

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -19,6 +19,7 @@
         function $(id){
             return document.getElementById(id);
         }
+
         function getSelectText()
         {
             var txt = null;
@@ -29,11 +30,34 @@
             }else if (document.selection){ //IE
                 txt = document.selection.createRange().text;
             }
-            //callAPI("123");
+
             return txt;
         }
 
-        //$("rs_081125").innerHTML = "<p>Call API</p>";
+        function callAPI(txt) {
+            console.log(txt);
+            var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+            url += '?' + $.param({
+                        'api-key': "d306567891f44528b129ce3f141317b9",
+                        'q': "trump"
+                    });
+            $.ajax({
+                url: url,
+                method: 'GET',
+                dataType: 'json'
+            }).done(function(response) {
+                if (response.status) {
+                    var doc = response.response.docs[0];
+                    var newsUrl = doc.web_url;
+                    var title = doc.headline.main;
+                    document.getElementById('news').innerHTML = "<br />"+title+"<br />"+newsUrl;
+                    //console.log(doc);
+                }
+                console.log(response);
+            }).fail(function(err) {
+                throw err;
+            });
+        }
     </script>
 
 </HEAD>
@@ -44,70 +68,17 @@
         <p>内容开始：As a result, I am pleased to announce a reorganization that will give some of our smaller business units greater scale. It will also allow us to integrate certain activities that we perform today in each unit into a country-wide organization. At the business level, we will consolidate six units into four: Healthcare, Consumer, Enterprise and Telecom (which now integrates Wireless) - all reporting to Sanjay Singh. We will also formalize the position of country heads in India, Ukraine and China - all reporting to Mukul Jain along with select global function heads. To be clear, these changes are not in anticipation of shrinkage but to better position us for growth. This new organization structure will ensure that our units are built around cohesive markets, which is good for our clients. It will also ensure that countries and product engineering centers will operate in a more integrated fashion, which is good for our employees. It will also support the more rapid adoption of global systems and processes. To learn more, please refer to the Re-organization Summary section below.</p>
     </div>
     <p>&nbsp;</p>
-    <div id="rs_081125" class="code">您选取的内容是:</div>
+    <div id="content" class="code">您选取的内容是:</div>
+    <div id="news" class="code">您选取的内容是:</div>
     <script type="text/javascript">
         /*<![CDATA[*/
         function f(){
             var txt = getSelectText();
             if(txt!=null){
-                $("rs_081125").innerHTML="您选取的内容是:<br />"+txt;
-                callapi2(txt);
+                document.getElementById('content').innerHTML = "您选取的内容是:<br />"+txt;
+                callAPI(txt);
             }
         } /*]]>*/
-        function callapi2(txt) {
-            // Built by LucyBot. www.lucybot.com
-            console.log(txt);
-            var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-            url += '?' + $.param({
-                        'api-key': "d306567891f44528b129ce3f141317b9",
-                        'q': "trump"
-                    });
-            $.ajax({
-                url: url,
-                method: 'GET',
-            }).done(function(result) {
-                $("rs_081125").innerHTML+=result;
-                console.log(result);
-            }).fail(function(err) {
-                throw err;
-            });
-        }
-        function callAPI(txt) {
-            // Built by LucyBot. www.lucybot.com
-            //return "test";
-            //$("rs_081125").innerHTML += "<p>"+txt+"</p>";
-            /*var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-             url += '?' + $.param({
-             'api-key': "d306567891f44528b129ce3f141317b9",
-             'q': txt
-             });*/
-            $.ajax({
-                url: 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
-                data: {
-                    'api-key': "d306567891f44528b129ce3f141317b9",
-                    'q': txt
-                },
-                type: 'GET',
-                success: function (response) {
-                    $("rs_081125").innerHTML += "<p>"+response+"</p>";
-                    //return response;
-                },
-                error: function (xhr, ajaxOptions, thrownError) {
-                    $("rs_081125").innerHTML += "<p>"+txt+"</p>";
-                    alert(xhr.status);
-                    alert(thrownError);
-                    //return response;
-                }
-            });/*.done(function(result) {
-             console.log(result);
-             $("rs_081125").innerHTML += "<p>S Call API</p>";
-             //return "test2";
-             }).fail(function(err) {
-             //return "test3";
-             //$("rs_081125").innerHTML = "<p>F Call API</p>";
-             throw err;
-             });*/
-        }
     </script>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" type="text/javascript"></script>
 
