fbf4db42194789badab8ea22e87baf43fe2a0878
Rita Lo <ritalo@Ritade-MacBook-Pro.local>
2017-04-17 22:47:17 +0800

    add for-loop to show 10 results for callAPI()

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -16,12 +16,11 @@
         }
     </style>
     <script type="text/javascript">
-        function $(id){
+        function $(id) {
             return document.getElementById(id);
         }
 
-        function getSelectText()
-        {
+        function getSelectText() {
             var txt = null;
             if (window.getSelection){ // mozilla FF
                 txt = window.getSelection();
@@ -45,14 +44,16 @@
                 dataType: 'json'
             }).done(function(response) {
                 if (response.status) {
-                    var doc = response.response.docs[0];
-                    var newsUrl = doc.web_url;
-                    var title = doc.headline.main;
-                    document.getElementById('news').innerHTML = "<br />"+title+"<br />"+newsUrl;
-                    //console.log(doc);
+                    document.getElementById('news').innerHTML = "New York Times Search Result:";
+                    var doc = response.response.docs;
+                    for (key in doc) {
+                        var title = doc[key].headline.main;
+                        var news = "<a href="+doc[key].web_url+">"+title+"</a>";
+                        document.getElementById('news').innerHTML += "<br />"+news+"<br/>";
+                    }
                 }
-                console.log(response);
             }).fail(function(err) {
+                alert(response);
                 document.getElementById('news').innerHTML = "<br />"+"找不到新聞"+"<br />";
                 throw err;
             });
@@ -64,16 +65,16 @@
 <BODY>
     <p>效果演示区</p>
     <div class="code" onmouseup="javascript:f()">
-        <p>内容开始：As a result, I am pleased to announce a reorganization that will give some of our smaller business units greater scale. It will also allow us to integrate certain activities that we perform today in each unit into a country-wide organization. At the business level, we will consolidate six units into four: Healthcare, Consumer, Enterprise and Telecom (which now integrates Wireless) - all reporting to Sanjay Singh. We will also formalize the position of country heads in India, Ukraine and China - all reporting to Mukul Jain along with select global function heads. To be clear, these changes are not in anticipation of shrinkage but to better position us for growth. This new organization structure will ensure that our units are built around cohesive markets, which is good for our clients. It will also ensure that countries and product engineering centers will operate in a more integrated fashion, which is good for our employees. It will also support the more rapid adoption of global systems and processes. To learn more, please refer to the Re-organization Summary section below.</p>
+        <p>内容开始：trump As a result, I am pleased to announce a reorganization that will give some of our smaller business units greater scale. It will also allow us to integrate certain activities that we perform today in each unit into a country-wide organization. At the business level, we will consolidate six units into four: Healthcare, Consumer, Enterprise and Telecom (which now integrates Wireless) - all reporting to Sanjay Singh. We will also formalize the position of country heads in India, Ukraine and China - all reporting to Mukul Jain along with select global function heads. To be clear, these changes are not in anticipation of shrinkage but to better position us for growth. This new organization structure will ensure that our units are built around cohesive markets, which is good for our clients. It will also ensure that countries and product engineering centers will operate in a more integrated fashion, which is good for our employees. It will also support the more rapid adoption of global systems and processes. To learn more, please refer to the Re-organization Summary section below.</p>
     </div>
     <p>&nbsp;</p>
     <div id="content" class="code">您选取的内容是:</div>
-    <div id="news" class="code">您选取的内容是:</div>
+    <div id="news" class="code">New York Times Search Result:</div>
     <script type="text/javascript">
         /*<![CDATA[*/
         function f(){
             var txt = getSelectText();
-            if(txt!=null){
+            if(txt!=null && txt != ''){
                 document.getElementById('content').innerHTML = "您选取的内容是:<br />"+txt;
                 callAPI(txt);
             }
